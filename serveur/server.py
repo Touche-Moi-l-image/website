c072431ae3b8ee6834049f07ed835fea16c2e3f2
Quentin <109299545+QuentinGrnr@users.noreply.github.com>
2025-11-04 10:53:01 +0100

    add blurring

--- serveur/server.py
+++ serveur/server.py
@@ -2,12 +2,14 @@ from flask import Flask
 from routes.convert_to_bw import convert_to_bw
 from routes.rotate_image import rotate_image
 from routes.flip_image import flip_image
+from routes.blur_image import blur_image
 
 app = Flask(__name__)
 
 app.register_blueprint(convert_to_bw)
 app.register_blueprint(rotate_image)
 app.register_blueprint(flip_image)
+app.register_blueprint(blur_image)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
