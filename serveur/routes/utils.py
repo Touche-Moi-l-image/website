f1cee88df5027e143c941eab2c72252bd04d214d
Lucas Rosenzweig <xeit0w@gmail.com>
2025-11-10 16:01:39 +0100

    fix : on attend du base 64 et pas un chemin absolu

--- serveur/routes/utils.py
+++ serveur/routes/utils.py
@@ -1,6 +1,8 @@
 from flask import jsonify, send_file
+from io import BytesIO
 from PIL import Image
 import io
+import base64
 import requests
 import os
 import traceback
@@ -8,28 +10,9 @@ import traceback
 def process_image(image_source, transform_function):
     try:
         print("Image source received:", image_source)
-        
-        # Convertir les chemins relatifs en chemins absolus
-        if not (image_source.startswith('http://') or image_source.startswith('https://')):
-            image_source = os.path.abspath(image_source)
-            print("Resolved absolute path:", image_source)
-        
-        if image_source.startswith('http://') or image_source.startswith('https://'):
-            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
-            response = requests.get(image_source, headers=headers)
-            response.raise_for_status()
-            image = Image.open(io.BytesIO(response.content))
-        else:
-            try:
-                with open(image_source, 'rb') as f:
-                    image = Image.open(f)
-            except FileNotFoundError:
-                print("FileNotFoundError: The file does not exist at path:", image_source)
-                raise
-            except Exception as e:
-                print("Error while opening the file:", str(e))
-                raise
-
+        image_source = image_source.split(',')[1]
+        image_data = base64.b64decode(image_source)
+        image = Image.open(BytesIO(image_data))
         image.load()
 
         try:
